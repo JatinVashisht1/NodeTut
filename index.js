ecb09bd42b8dd67eba0bdc1531f593b211a21278
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-02-24 14:17:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello world');
+
+// Single thread can manage multiple connections very efficiently
+// It gets possible because of non-blocking IO model
+// Non-Blocking IO Model: It works on the principle that if one process is taking time to get complete, it will
+// not block the whole thread instead it will execute next process and the previous process will resume when it is ready to go.
+// This helps in handling more than one connections simultaneously in single thread only.
+
+// npm: node package manager, it manages packages of node project (similar to pip in python)
