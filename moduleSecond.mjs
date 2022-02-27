7dd3d6aeb3410926046084d8ac427d633ed36146
Jatin <jatin8085vashisht@gmail.com>
2022-02-27 12:59:40 +0530

    default export in node js

--- moduleSecond.mjs
+++ moduleSecond.mjs
@@ -1,7 +1,15 @@
-function Simple(){
-    console.log("Simple is complex");
+
+// to export function acc. to es6 we just have to write export keyword before function
+export function Simple1(){
+    console.log("Simple1 is complex");
+}
+
+// this has now become a default export function
+export default function Simple2(){
+    console.log("Simple2 is complex");
 }
 
 // below statement is also giving error after changing extension of this file to .mjs
 // so we have to use do it as described by es6 guidlines
-module.exports = Simple;
+// ES6 is the *RECOMMENDED* way to do import and export
+// export default Simple;
